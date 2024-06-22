0c938e3fe32a259947e377bed6b34fcd83a2db4b
jpmayoute <jeanphilippe.mayoute@gmail.com>
2024-06-22 20:11:35 +0200

    dynamisation browse ok

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -5,15 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PlayerRepository;
 
 class PlayerController extends AbstractController
 {
     #[Route('/player', name: 'app_player_browse', methods:"GET")]
     #[Route('/', name: 'app_player_home', methods:"GET")]
-    public function browse(): Response
+    public function browse(PlayerRepository $playerRepo): Response
     {
-        return $this->render('player/browse.html.twig');
+
+        //préparer les données
+        //ici récupérer les palyers
+        //donc on veut faire un select => Repository
+        $allPlayers = $playerRepo->findAll();
+        //dd($allPlayers);
+
+
+        return $this->render('player/browse.html.twig', [
+            'playerList' => $allPlayers,
+        ]);
     }
+
+
+
 
     #[Route('/player/{id}', name: 'app_player_read', methods:"GET", requirements: ["id" => "\d+"])]
     public function read(): Response
--- src/Entity/Position.php
+++ src/Entity/Position.php
@@ -75,4 +75,9 @@ class Position
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->position_name ?? '';
+    }
 }
